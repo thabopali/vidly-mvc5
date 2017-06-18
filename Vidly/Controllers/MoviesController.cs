d933e3f78fcc8a10feb2648a141bbaf792ad5adb
thabopali <pali.thabo@gmail.com>
2017-06-18 20:55:28 +0200

    AddMoviesWithGenres

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Configuration;
 using System.Web.Mvc;
+using System.Data.Entity;
 using Vidly.Models;
 using Vidly.ViewModels;
 
@@ -12,18 +13,35 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
 
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
 
         public ViewResult Index()
         {
-           var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
  
             return View(movies);    
-       }
-
-        private object GetMovies()
-        {
-            throw new NotImplementedException();
         }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
 
       
 
